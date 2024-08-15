53c1d073e759632b5a8dbad754ffe839432cec0f
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-08-15 13:56:30 -0600

    Create class to connect to DB, no running yet

---
+++ controllers/db.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+class MongoDBConnection:
+    def __init__(self, db_name, host='mongodb://localhost', port=27017):
+        self.client = MongoClient(host, port)
+        self.db = self.client[db_name]
+
+    def get_notes_db(self, collection_name):
+        return self.db[collection_name]
--- controllers/operations.py
+++ controllers/operations.py
@@ -1,4 +1,11 @@
+from controllers.db import MongoDBConnection
+
+database_name='todoappdb'
+db = MongoDBConnection(database_name)
+
 class MongoDBController:
-    def get_notes(self):#it is important to send "self" as parameter event when the function does not recieve real args. This is
-    # a Flask requisite
-        return "Notes are: []"
+    #it is important to send "self" as parameter event when the function does not recieve real args. This is a Flask requisite
+    def get_notes(self):
+        collection_name='todoappcollection'
+        all_notes_from_db = db.get_notes_db(collection_name)
+        return all_notes_from_db
